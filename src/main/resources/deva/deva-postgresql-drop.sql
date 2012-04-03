31428a0bbfb071a59dab78191325b7cdea311b9d
Markus Dahm <markus.dahm@akquinet.de>
2012-04-03 08:11:12 +0000

    no trigger

--- src/main/resources/deva/deva-postgresql-drop.sql
+++ src/main/resources/deva/deva-postgresql-drop.sql
@@ -403,3 +403,17 @@ DROP TABLE "public"."drools_sessioninfo" CASCADE;
 DROP TABLE "public"."deva_faelligkeiten" CASCADE;
 
 DROP TABLE "public"."deva_dokument_binaerdaten" CASCADE;
+
+
+create or replace function drop_all_sequences() returns integer as '
+declare
+ rec record;
+begin
+ for rec in select relname as seqname
+   from pg_class where relkind=''S''
+ loop
+   execute ''drop sequence '' || rec.seqname;
+ end loop;
+ return 1;
+end;
+' language 'plpgsql';
