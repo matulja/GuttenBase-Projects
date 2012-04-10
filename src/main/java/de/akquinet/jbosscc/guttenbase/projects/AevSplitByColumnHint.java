236ef8b2caad7885b8f14e4d4e914d3748b9bf8d
Markus Dahm <markus.dahm@akquinet.de>
2012-04-10 09:03:36 +0000

    renamed

--- src/main/java/de/akquinet/jbosscc/guttenbase/projects/AevSplitByColumnHint.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/AevSplitByColumnHint.java
@@ -1,15 +1,15 @@
 package de.akquinet.jbosscc.guttenbase.projects;
 
-import de.akquinet.jbosscc.guttenbase.hints.SplitByColumn;
-import de.akquinet.jbosscc.guttenbase.hints.SplitByColumnHint;
+import de.akquinet.jbosscc.guttenbase.hints.SplitColumn;
+import de.akquinet.jbosscc.guttenbase.hints.SplitColumnHint;
 import de.akquinet.jbosscc.guttenbase.repository.TableMetaData;
 
-public final class AevSplitByColumnHint extends SplitByColumnHint {
+public final class AevSplitByColumnHint extends SplitColumnHint {
   private static final long serialVersionUID = 1L;
 
   @Override
-  public SplitByColumn getValue() {
-    return new SplitByColumn() {
+  public SplitColumn getValue() {
+    return new SplitColumn() {
       @Override
       public String getSplitColumn(final TableMetaData table) {
         final String lowerCase = table.getTableName().toLowerCase();
--- src/main/java/de/akquinet/jbosscc/guttenbase/projects/MeyleImportDeva.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/MeyleImportDeva.java
@@ -5,10 +5,13 @@ import java.sql.SQLException;
 import org.apache.log4j.Logger;
 
 import de.akquinet.jbosscc.guttenbase.export.ImportDumpConnectionInfo;
+import de.akquinet.jbosscc.guttenbase.hints.SplitColumn;
+import de.akquinet.jbosscc.guttenbase.hints.SplitColumnHint;
 import de.akquinet.jbosscc.guttenbase.repository.ConnectorRepository;
 import de.akquinet.jbosscc.guttenbase.repository.impl.ConnectorRepositoryImpl;
 import de.akquinet.jbosscc.guttenbase.tools.DefaultTableCopier;
 import de.akquinet.jbosscc.guttenbase.tools.TableConfigurationChecker;
+import de.akquinet.jbosscc.guttenbase.tools.postgresql.PostgresqlSequenceUpdater;
 
 public class MeyleImportDeva {
   private static final Logger LOG = Logger.getLogger(KvbbCopyAev.class);
@@ -32,6 +35,17 @@ public class MeyleImportDeva {
 
       new TableConfigurationChecker(connectorRepository).checkTableConfiguration("meyleImport", "meylePostgresql");
       new DefaultTableCopier(connectorRepository).copyTables("meyleImport", "meylePostgresql");
+
+      connectorRepository.addConnectionHint("meylePostgresql", new SplitColumnHint() {
+
+        @Override
+        public SplitColumn getValue() {
+          // TODO Auto-generated method stub
+          return null;
+        }
+      });
+
+      new PostgresqlSequenceUpdater(connectorRepository).updateSequences("meylePostgresql");
     } catch (final SQLException e) {
       LOG.error("main", e);
     }
