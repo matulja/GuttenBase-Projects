0ef0d2bd45ddb18c10e719aa70f8af8765e84f5a
Markus Dahm <markus.dahm@akquinet.de>
2012-04-26 09:48:54 +0000

    format time executeUpdate()

---
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/tdm/IdColumnMapper.java
@@ -0,0 +1,53 @@
+package de.akquinet.jbosscc.guttenbase.projects.tdm;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import de.akquinet.jbosscc.guttenbase.mapping.ColumnDataMapper;
+import de.akquinet.jbosscc.guttenbase.meta.ColumnMetaData;
+
+public final class IdColumnMapper implements ColumnDataMapper {
+  private final Map<String, String> _idMap = new HashMap<String, String>();
+
+  public String createKey(final ColumnMetaData columnMetaData, final long id) {
+    final String key = columnMetaData.getTableMetaData().getTableName() + ":" + columnMetaData.getColumnName() + ":" + id;
+    String newId = _idMap.get(key);
+
+    if (newId == null) {
+      newId = UUID.randomUUID().toString();
+      _idMap.put(key, newId);
+    }
+
+    return newId;
+  }
+
+  @Override
+  public Object map(final ColumnMetaData sourceColumnMetaData, final ColumnMetaData targetColumnMetaData, final Object value) {
+    final Number number = (Number) value;
+
+    if (number != null) {
+      final long id = number.longValue();
+      final ColumnMetaData referencedColumn = sourceColumnMetaData.getReferencedColumn();
+      String uuid;
+
+      if (referencedColumn != null) {
+        uuid = createKey(referencedColumn, id);
+      } else {
+        uuid = createKey(sourceColumnMetaData, id);
+      }
+
+      return uuid;
+    } else {
+      return null;
+    }
+  }
+
+  @Override
+  public boolean isApplicable(final ColumnMetaData sourceColumnMetaData, final ColumnMetaData targetColumnMetaData) {
+    final String columnName = sourceColumnMetaData.getColumnName().toLowerCase();
+    final boolean columnNamesEqual = columnName.equalsIgnoreCase(targetColumnMetaData.getColumnName());
+
+    return columnNamesEqual && (columnName.endsWith("_id") || "id".equals(columnName));
+  }
+}
--- src/main/java/de/akquinet/jbosscc/guttenbase/projects/tdm/TdmKaCopyWithUUID.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/tdm/TdmKaCopyWithUUID.java
@@ -1,72 +1,17 @@
 package de.akquinet.jbosscc.guttenbase.projects.tdm;
 
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 import org.apache.log4j.Logger;
 
 import de.akquinet.jbosscc.guttenbase.hints.impl.DefaultColumnDataMapperFactory;
 import de.akquinet.jbosscc.guttenbase.hints.impl.DefaultColumnDataMapperFactoryHint;
-import de.akquinet.jbosscc.guttenbase.mapping.ColumnDataMapper;
-import de.akquinet.jbosscc.guttenbase.meta.ColumnMetaData;
 import de.akquinet.jbosscc.guttenbase.meta.ColumnType;
 import de.akquinet.jbosscc.guttenbase.repository.ConnectorRepository;
 import de.akquinet.jbosscc.guttenbase.repository.impl.ConnectorRepositoryImpl;
 import de.akquinet.jbosscc.guttenbase.tools.DefaultTableCopier;
-import de.akquinet.jbosscc.guttenbase.tools.TableConfigurationChecker;
-import de.akquinet.jbosscc.guttenbase.tools.TableDataChecker;
-import de.akquinet.jbosscc.guttenbase.tools.postgresql.PostgresqlReindexTablesTool;
-import de.akquinet.jbosscc.guttenbase.tools.postgresql.PostgresqlVacuumTablesTool;
 
 public class TdmKaCopyWithUUID {
-
-  public static final class IdColumnMapper implements ColumnDataMapper {
-    private final Map<String, String> _idMap = new HashMap<String, String>();
-
-    public String createKey(final ColumnMetaData columnMetaData, final long id) {
-      final String key = columnMetaData.getTableMetaData().getTableName() + ":" + columnMetaData.getColumnName() + ":" + id;
-      String newId = _idMap.get(key);
-
-      if (newId == null) {
-        newId = UUID.randomUUID().toString();
-        _idMap.put(key, newId);
-      }
-
-      return newId;
-    }
-
-    @Override
-    public Object map(final ColumnMetaData sourceColumnMetaData, final ColumnMetaData targetColumnMetaData, final Object value) {
-      final Number number = (Number) value;
-
-      if (number != null) {
-        final long id = number.longValue();
-        final ColumnMetaData referencedColumn = sourceColumnMetaData.getReferencedColumn();
-        String uuid;
-
-        if (referencedColumn != null) {
-          uuid = createKey(referencedColumn, id);
-        } else {
-          uuid = createKey(sourceColumnMetaData, id);
-        }
-
-        return uuid;
-      } else {
-        return null;
-      }
-    }
-
-    @Override
-    public boolean isApplicable(final ColumnMetaData sourceColumnMetaData, final ColumnMetaData targetColumnMetaData) {
-      final String columnName = sourceColumnMetaData.getColumnName().toLowerCase();
-      final boolean columnNamesEqual = columnName.equalsIgnoreCase(targetColumnMetaData.getColumnName());
-
-      return columnNamesEqual && (columnName.endsWith("_id") || "id".equals(columnName));
-    }
-  }
-
   private static final String TARGET = "TARGET";
   private static final String SOURCE = "SOURCE";
   private static final Logger LOG = Logger.getLogger(TdmKaCopyWithUUID.class);
@@ -91,12 +36,14 @@ public class TdmKaCopyWithUUID {
       });
 
       // new PostgresqlReindexTablesTool(connectorRepository).executeOnAllTables(SOURCE);
+      // new PostgresqlVacuumTablesTool(connectorRepository).executeOnAllTables(SOURCE);
 
-      new TableConfigurationChecker(connectorRepository).checkTableConfiguration(SOURCE, TARGET);
+      // new TableConfigurationChecker(connectorRepository).checkTableConfiguration(SOURCE, TARGET);
       new DefaultTableCopier(connectorRepository).copyTables(SOURCE, TARGET);
-      new TableDataChecker(connectorRepository).checkTableData(SOURCE, TARGET);
-      new PostgresqlReindexTablesTool(connectorRepository).executeOnAllTables(TARGET);
-      new PostgresqlVacuumTablesTool(connectorRepository).executeOnAllTables(TARGET);
+      // new TableDataChecker(connectorRepository).checkTableData(SOURCE, TARGET);
+
+      // new PostgresqlVacuumTablesTool(connectorRepository).executeOnAllTables(TARGET);
+      // new PostgresqlReindexTablesTool(connectorRepository).executeOnAllTables(TARGET);
     } catch (final SQLException e) {
       LOG.error("main", e);
     }
