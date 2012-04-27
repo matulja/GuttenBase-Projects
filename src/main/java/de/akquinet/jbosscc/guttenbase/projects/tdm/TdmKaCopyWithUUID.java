ef10cb9dbec78419b74f42f242687773bcbf3b3e
Markus Dahm <markus.dahm@akquinet.de>
2012-04-27 07:45:50 +0000

    foo

--- src/main/java/de/akquinet/jbosscc/guttenbase/projects/tdm/TdmKaCopyWithUUID.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/tdm/TdmKaCopyWithUUID.java
@@ -4,16 +4,19 @@ import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
 
+import de.akquinet.jbosscc.guttenbase.export.ExportDumpConnectionInfo;
+import de.akquinet.jbosscc.guttenbase.export.ImportDumpConnectionInfo;
 import de.akquinet.jbosscc.guttenbase.hints.impl.DefaultColumnDataMapperFactory;
 import de.akquinet.jbosscc.guttenbase.hints.impl.DefaultColumnDataMapperFactoryHint;
 import de.akquinet.jbosscc.guttenbase.meta.ColumnType;
 import de.akquinet.jbosscc.guttenbase.repository.ConnectorRepository;
 import de.akquinet.jbosscc.guttenbase.repository.impl.ConnectorRepositoryImpl;
 import de.akquinet.jbosscc.guttenbase.tools.DefaultTableCopier;
-import de.akquinet.jbosscc.guttenbase.tools.postgresql.PostgresqlVacuumTablesTool;
 
 public class TdmKaCopyWithUUID {
   private static final String TARGET = "TARGET";
+  private static final String EXPORT = "DUMP";
+  private static final String IMPORT = "IMPORT";
   private static final String SOURCE = "SOURCE";
   private static final Logger LOG = Logger.getLogger(TdmKaCopyWithUUID.class);
 
@@ -23,6 +26,8 @@ public class TdmKaCopyWithUUID {
 
       connectorRepository.addConnectionInfo(SOURCE, new TdmKaPostgresqlConnectionInfo());
       connectorRepository.addConnectionInfo(TARGET, new TdmKaPostgresqlConnectionInfo2());
+      connectorRepository.addConnectionInfo(EXPORT, new ExportDumpConnectionInfo(SOURCE, "tdmka.jar"));
+      connectorRepository.addConnectionInfo(IMPORT, new ImportDumpConnectionInfo("tdmka.jar"));
 
       connectorRepository.addConnectorHint(SOURCE, new TdmKaTableNameFilterHint());
 
@@ -40,8 +45,9 @@ public class TdmKaCopyWithUUID {
       // new PostgresqlVacuumTablesTool(connectorRepository).executeOnAllTables(SOURCE);
 
       // new TableConfigurationChecker(connectorRepository).checkTableConfiguration(SOURCE, TARGET);
-      new PostgresqlVacuumTablesTool(connectorRepository).executeOnAllTables(TARGET);
-      new DefaultTableCopier(connectorRepository).copyTables(SOURCE, TARGET);
+      // new PostgresqlVacuumTablesTool(connectorRepository).executeOnAllTables(TARGET);
+      // new DefaultTableCopier(connectorRepository).copyTables(SOURCE, DUMP);
+      new DefaultTableCopier(connectorRepository).copyTables(IMPORT, TARGET);
       // new TableDataChecker(connectorRepository).checkTableData(SOURCE, TARGET);
 
       // new PostgresqlReindexTablesTool(connectorRepository).executeOnAllTables(TARGET);
