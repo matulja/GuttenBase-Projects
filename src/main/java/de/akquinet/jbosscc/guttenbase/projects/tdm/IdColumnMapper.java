6f8690c51fd0796b4c27d9e9d8e1f6caac88551e
Markus Dahm <markus.dahm@akquinet.de>
2012-05-15 13:55:51 +0000

    private

--- src/main/java/de/akquinet/jbosscc/guttenbase/projects/tdm/IdColumnMapper.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/tdm/IdColumnMapper.java
@@ -8,46 +8,46 @@ import de.akquinet.jbosscc.guttenbase.mapping.ColumnDataMapper;
 import de.akquinet.jbosscc.guttenbase.meta.ColumnMetaData;
 
 public final class IdColumnMapper implements ColumnDataMapper {
-  private final Map<String, String> _idMap = new HashMap<String, String>();
+	private final Map<String, String> _idMap = new HashMap<String, String>();
 
-  public String createKey(final ColumnMetaData columnMetaData, final long id) {
-    final String key = columnMetaData.getTableMetaData().getTableName() + ":" + columnMetaData.getColumnName() + ":" + id;
-    String newId = _idMap.get(key);
+	@Override
+	public Object map(final ColumnMetaData sourceColumnMetaData, final ColumnMetaData targetColumnMetaData, final Object value) {
+		final Number number = (Number) value;
 
-    if (newId == null) {
-      newId = UUID.randomUUID().toString();
-      _idMap.put(key, newId);
-    }
+		if (number != null) {
+			final long id = number.longValue();
+			final ColumnMetaData referencedColumn = sourceColumnMetaData.getReferencedColumn();
+			String uuid;
 
-    return newId;
-  }
+			if (referencedColumn != null) {
+				uuid = createKey(referencedColumn, id);
+			} else {
+				uuid = createKey(sourceColumnMetaData, id);
+			}
 
-  @Override
-  public Object map(final ColumnMetaData sourceColumnMetaData, final ColumnMetaData targetColumnMetaData, final Object value) {
-    final Number number = (Number) value;
+			return uuid;
+		} else {
+			return null;
+		}
+	}
 
-    if (number != null) {
-      final long id = number.longValue();
-      final ColumnMetaData referencedColumn = sourceColumnMetaData.getReferencedColumn();
-      String uuid;
+	@Override
+	public boolean isApplicable(final ColumnMetaData sourceColumnMetaData, final ColumnMetaData targetColumnMetaData) {
+		final String columnName = sourceColumnMetaData.getColumnName().toLowerCase();
+		final boolean columnNamesEqual = columnName.equalsIgnoreCase(targetColumnMetaData.getColumnName());
 
-      if (referencedColumn != null) {
-        uuid = createKey(referencedColumn, id);
-      } else {
-        uuid = createKey(sourceColumnMetaData, id);
-      }
+		return columnNamesEqual && (columnName.endsWith("_id") || "id".equals(columnName));
+	}
 
-      return uuid;
-    } else {
-      return null;
-    }
-  }
+	private String createKey(final ColumnMetaData columnMetaData, final long id) {
+		final String key = columnMetaData.getTableMetaData().getTableName() + ":" + columnMetaData.getColumnName() + ":" + id;
+		String newId = _idMap.get(key);
 
-  @Override
-  public boolean isApplicable(final ColumnMetaData sourceColumnMetaData, final ColumnMetaData targetColumnMetaData) {
-    final String columnName = sourceColumnMetaData.getColumnName().toLowerCase();
-    final boolean columnNamesEqual = columnName.equalsIgnoreCase(targetColumnMetaData.getColumnName());
+		if (newId == null) {
+			newId = UUID.randomUUID().toString();
+			_idMap.put(key, newId);
+		}
 
-    return columnNamesEqual && (columnName.endsWith("_id") || "id".equals(columnName));
-  }
+		return newId;
+	}
 }
