b62e05b20184cc533a36c3ccce5cffa5f5f8d4b5
Markus Dahm <markus.dahm@akquinet.de>
2012-05-29 08:26:41 +0000

    compact update

--- src/main/java/de/akquinet/jbosscc/guttenbase/projects/deva/MeyleImportDeva.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/deva/MeyleImportDeva.java
@@ -12,37 +12,37 @@ import de.akquinet.jbosscc.guttenbase.tools.ScriptExecutor;
 import de.akquinet.jbosscc.guttenbase.tools.TableConfigurationChecker;
 
 public class MeyleImportDeva {
-  private static final String TARGET = "meylePostgresql";
-  private static final String SOURCE = "meyleImport";
-  private static final Logger LOG = Logger.getLogger(MeyleImportDeva.class);
+	private static final String TARGET = "meylePostgresql";
+	private static final String SOURCE = "meyleImport";
+	private static final Logger LOG = Logger.getLogger(MeyleImportDeva.class);
 
-  public static void main(final String[] args) {
-    try {
-      final ConnectorRepository connectorRepository = new ConnectorRepositoryImpl();
+	public static void main(final String[] args) {
+		try {
+			final ConnectorRepository connectorRepository = new ConnectorRepositoryImpl();
 
-      connectorRepository.addConnectionInfo(SOURCE, new ImportDumpConnectionInfo("deva.jar"));
-      connectorRepository.addConnectionInfo(TARGET, new MeylePostgresqlConnectionInfo());
-      connectorRepository.addConnectorHint(SOURCE, new MeyleTableNameFilterHint(true, true));
-      connectorRepository.addConnectorHint(TARGET, new MeyleTableNameFilterHint(true, true));
+			connectorRepository.addConnectionInfo(SOURCE, new ImportDumpConnectionInfo("deva.jar"));
+			connectorRepository.addConnectionInfo(TARGET, new MeylePostgresqlConnectionInfo());
+			connectorRepository.addConnectorHint(SOURCE, new MeyleTableNameFilterHint(true, true));
+			connectorRepository.addConnectorHint(TARGET, new MeyleTableNameFilterHint(true, true));
 
-      try {
-        new ScriptExecutor(connectorRepository).executeFileScript("meylePostgresql", "deva/deva-postgresql-drop.sql");
-      } catch (final SQLException e) {
-        LOG.error("drop", e);
-      }
+			try {
+				new ScriptExecutor(connectorRepository).executeFileScript("meylePostgresql", "deva/deva-postgresql-drop.sql");
+			} catch (final SQLException e) {
+				LOG.error("drop", e);
+			}
 
-      new ScriptExecutor(connectorRepository).executeFileScript("meylePostgresql", "deva/deva-postgresql.ddl");
+			new ScriptExecutor(connectorRepository).executeFileScript("meylePostgresql", "deva/deva-postgresql.ddl");
 
-      new TableConfigurationChecker(connectorRepository).checkTableConfiguration("meyleImport", "meylePostgresql");
-      new DefaultTableCopier(connectorRepository).copyTables("meyleImport", "meylePostgresql");
+			new TableConfigurationChecker(connectorRepository).checkTableConfiguration("meyleImport", "meylePostgresql");
+			new DefaultTableCopier(connectorRepository).copyTables("meyleImport", "meylePostgresql");
 
-      connectorRepository.addConnectorHint(TARGET, new MeyleTableNameFilterHint(false, false));
-      new MeylePostgresqlSequenceUpdater(connectorRepository).updateSequences(TARGET);
+			connectorRepository.addConnectorHint(TARGET, new MeyleTableNameFilterHint(false, false));
+			new MeylePostgresqlSequenceUpdater(connectorRepository).updateSequences(TARGET);
 
-      new ScriptExecutor(connectorRepository).executeScript(TARGET, "SELECT setval('public.sessioninfo_id_seq', 351, true);",
-          "SELECT setval('public.workiteminfo_id_seq', 651, true);", "SELECT setval('public.hibernate_sequence', 259, true);");
-    } catch (final SQLException e) {
-      LOG.error("main", e);
-    }
-  }
+			new ScriptExecutor(connectorRepository).executeScript(TARGET, false, false, "SELECT setval('public.sessioninfo_id_seq', 351, true);",
+					"SELECT setval('public.workiteminfo_id_seq', 651, true);", "SELECT setval('public.hibernate_sequence', 259, true);");
+		} catch (final SQLException e) {
+			LOG.error("main", e);
+		}
+	}
 }
