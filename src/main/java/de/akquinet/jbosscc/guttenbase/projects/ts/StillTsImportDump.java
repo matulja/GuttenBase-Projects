18e0ab44a258167d45a895fd7a35464544b2d43a
Markus Dahm <markus.dahm@akquinet.de>
2012-05-29 14:33:51 +0000

    udpate

--- src/main/java/de/akquinet/jbosscc/guttenbase/projects/ts/StillTsImportDump.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/ts/StillTsImportDump.java
@@ -8,7 +8,6 @@ import org.apache.log4j.Logger;
 
 import de.akquinet.jbosscc.guttenbase.export.ImportDumpConnectionInfo;
 import de.akquinet.jbosscc.guttenbase.hints.ColumnMapperHint;
-import de.akquinet.jbosscc.guttenbase.hints.DatabaseTableFilterHint;
 import de.akquinet.jbosscc.guttenbase.hints.TableMapperHint;
 import de.akquinet.jbosscc.guttenbase.mapping.ColumnMapper;
 import de.akquinet.jbosscc.guttenbase.mapping.TableMapper;
@@ -16,7 +15,6 @@ import de.akquinet.jbosscc.guttenbase.meta.ColumnMetaData;
 import de.akquinet.jbosscc.guttenbase.meta.DatabaseMetaData;
 import de.akquinet.jbosscc.guttenbase.meta.TableMetaData;
 import de.akquinet.jbosscc.guttenbase.repository.ConnectorRepository;
-import de.akquinet.jbosscc.guttenbase.repository.DatabaseTableFilter;
 import de.akquinet.jbosscc.guttenbase.repository.impl.ConnectorRepositoryImpl;
 import de.akquinet.jbosscc.guttenbase.tools.DefaultTableCopier;
 
@@ -32,25 +30,19 @@ public class StillTsImportDump {
 			connectorRepository.addConnectionInfo(IMPORT, new ImportDumpConnectionInfo("stillts.jar"));
 			connectorRepository.addConnectionInfo(TARGET, new StillTsPostgresqlConnectionInfo());
 
-			// 17:59:22,903 INFO [AbstractTableCopier] 26 lines copied, average per batch (2000) = 00:00:00
-			// T=84, r=114, a=97, g=103, f=102, õ=228(245), h=104, i=105, g=103, k=107, e=101, i=105, t=116, s=115, s=115, c=99, h=104, i=105,
-			// l=108,
-			// d=100, e=101, r=114,
-			// 17:59:22,905 INFO [AbstractTableCopier] Copying of `Tragfõhigkeitsschilder`-> Tragfõhigkeitsschilder(36/39) started
-
-			connectorRepository.addConnectorHint(IMPORT, new DatabaseTableFilterHint() {
-				@Override
-				public DatabaseTableFilter getValue() {
-					return new DatabaseTableFilter() {
-
-						@Override
-						public boolean accept(final TableMetaData table) throws SQLException {
-							final String lowerCase = table.getTableName().toLowerCase();
-							return lowerCase.compareTo("tragflayoutsachnummern") >= 0;
-						}
-					};
-				}
-			});
+			// connectorRepository.addConnectorHint(IMPORT, new DatabaseTableFilterHint() {
+			// @Override
+			// public DatabaseTableFilter getValue() {
+			// return new DatabaseTableFilter() {
+			//
+			// @Override
+			// public boolean accept(final TableMetaData table) throws SQLException {
+			// final String lowerCase = table.getTableName().toLowerCase();
+			// return lowerCase.compareTo("tragfähigkeitsschilder") >= 0;
+			// }
+			// };
+			// }
+			// });
 
 			connectorRepository.addConnectorHint(TARGET, new TableMapperHint() {
 				@Override
