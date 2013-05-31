5dc74d24a7029ff76f631d81d30c1ceed196997e
Markus Dahm <markus.dahm@akquinet.de>
2013-05-31 15:01:12 +0000

    minimize data

--- src/main/java/de/akquinet/jbosscc/guttenbase/projects/deva/MeyleShrinkDump.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/deva/MeyleShrinkDump.java
@@ -1,15 +1,19 @@
 package de.akquinet.jbosscc.guttenbase.projects.deva;
 
 import java.io.File;
+import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
 
 import de.akquinet.jbosscc.guttenbase.export.ExportDumpConnectorInfo;
 import de.akquinet.jbosscc.guttenbase.export.ImportDumpConnectionInfo;
+import de.akquinet.jbosscc.guttenbase.hints.RepositoryTableFilterHint;
 import de.akquinet.jbosscc.guttenbase.hints.impl.DefaultColumnDataMapperProvider;
 import de.akquinet.jbosscc.guttenbase.hints.impl.DefaultColumnDataMapperProviderHint;
 import de.akquinet.jbosscc.guttenbase.meta.ColumnType;
+import de.akquinet.jbosscc.guttenbase.meta.TableMetaData;
 import de.akquinet.jbosscc.guttenbase.repository.ConnectorRepository;
+import de.akquinet.jbosscc.guttenbase.repository.RepositoryTableFilter;
 import de.akquinet.jbosscc.guttenbase.repository.impl.ConnectorRepositoryImpl;
 import de.akquinet.jbosscc.guttenbase.tools.DefaultTableCopyTool;
 
@@ -23,7 +27,7 @@ public class MeyleShrinkDump {
       final ConnectorRepository connectorRepository = new ConnectorRepositoryImpl();
 
       connectorRepository.addConnectionInfo(SOURCE, new ImportDumpConnectionInfo(new File("deva-postgresql.jar").toURI().toURL()));
-      connectorRepository.addConnectionInfo(TARGET, new ExportDumpConnectorInfo(SOURCE, "deva-shrunk.jar"));
+      connectorRepository.addConnectionInfo(TARGET, new ExportDumpConnectorInfo(SOURCE, "deva-minimal.jar"));
 
       connectorRepository.addConnectorHint(TARGET, new DefaultColumnDataMapperProviderHint() {
         @Override
@@ -32,6 +36,20 @@ public class MeyleShrinkDump {
           columnDataMapperFactory.addMapping(ColumnType.CLASS_BLOB, ColumnType.CLASS_BLOB, new MeyleBlobDataMapper());
         }
       });
+
+      final RepositoryTableFilterHint filterHint = new RepositoryTableFilterHint() {
+        @Override
+        public RepositoryTableFilter getValue() {
+          return new RepositoryTableFilter() {
+            @Override
+            public boolean accept(final TableMetaData table) throws SQLException {
+              final String tableName = table.getTableName().toUpperCase();
+              return !tableName.endsWith("LOGBUCH");
+            }
+          };
+        }
+      };
+      connectorRepository.addConnectorHint(SOURCE, filterHint);
 
       new DefaultTableCopyTool(connectorRepository).copyTables(SOURCE, TARGET);
     } catch (final Exception e) {
