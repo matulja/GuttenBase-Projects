330d737f4b2a804cc949e6c98fdbe32f77cbb5d7
Markus Dahm <markus.dahm@akquinet.de>
2013-10-01 14:22:06 +0000

    fix

--- src/main/java/de/akquinet/jbosscc/guttenbase/projects/tdm/TdmKaCopy.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/tdm/TdmKaCopy.java
@@ -21,120 +21,141 @@ import de.akquinet.jbosscc.guttenbase.tools.DefaultTableCopyTool;
 import de.akquinet.jbosscc.guttenbase.tools.ScriptExecutorTool;
 import de.akquinet.jbosscc.guttenbase.utils.DatabaseSchemaScriptCreator;
 
-public class TdmKaCopy {
-	private static final String SOURCE = "SOURCE";
-	private static final String TARGET = "TARGET";
-	// private static final String IMPORT = "IMPORT";
-	private static final String TARGET_NEW_TABLES = "TARGET_NEW_TABLES";
+public class TdmKaCopy
+{
+  private static final String SOURCE = "SOURCE";
+  private static final String TARGET = "TARGET";
+  // private static final String IMPORT = "IMPORT";
+  private static final String TARGET_NEW_TABLES = "TARGET_NEW_TABLES";
 
-	private static final Logger LOG = Logger.getLogger(TdmKaCopy.class);
+  private static final Logger LOG = Logger.getLogger(TdmKaCopy.class);
 
-	public static void main(final String[] args) {
-		try {
-			final ConnectorRepository connectorRepository = new ConnectorRepositoryImpl();
-			final TdmKaPostgresqlConnectionInfo sourceInfo = new TdmKaPostgresqlConnectionInfo();
-			final TdmKaPostgresqlConnectionInfo2 targetInfo = new TdmKaPostgresqlConnectionInfo2();
+  public static void main(final String[] args)
+  {
+    try
+    {
+      final ConnectorRepository connectorRepository = new ConnectorRepositoryImpl();
+      final TdmKaPostgresqlConnectionInfo sourceInfo = new TdmKaPostgresqlConnectionInfo();
+      final TdmKaPostgresqlConnectionInfo2 targetInfo = new TdmKaPostgresqlConnectionInfo2();
 
-			connectorRepository.addConnectionInfo(SOURCE, sourceInfo);
-			connectorRepository.addConnectionInfo(TARGET, targetInfo);
-			connectorRepository.addConnectorHint(SOURCE, new TdmKaSourceTableNameFilterHint());
-			connectorRepository.addConnectorHint(TARGET, new TdmKaSourceTableNameFilterHint());
+      connectorRepository.addConnectionInfo(SOURCE, sourceInfo);
+      connectorRepository.addConnectionInfo(TARGET, targetInfo);
+      connectorRepository.addConnectorHint(SOURCE, new TdmKaSourceTableNameFilterHint());
+      connectorRepository.addConnectorHint(TARGET, new TdmKaSourceTableNameFilterHint());
 
-			new ScriptExecutorTool(connectorRepository).executeFileScript(TARGET, "/tdmka/tdmka-uuid-postgresql.ddl");
+      new ScriptExecutorTool(connectorRepository).executeFileScript(TARGET, "/tdmka/tdmka-uuid-postgresql.ddl");
 
-			// Create insert statements needed later with ALL column
-			final List<String> insertStatements = new TdmKaInsertSelectStatementCreator(connectorRepository, SOURCE, sourceInfo.getSchema(),
-					targetInfo.getSchema()).createInsertStatements();
+      // Create insert statements needed later with ALL column
+      final List<String> insertStatements = new TdmKaInsertSelectStatementCreator(connectorRepository, SOURCE,
+          sourceInfo.getSchema(), targetInfo.getSchema()).createInsertStatements();
 
-			// Create new "database", i.e. new tables used for migration
-			final DatabaseMetaData mappingDatabaseMetaData = new TdmKaMappingTablesCreator(connectorRepository, SOURCE, targetInfo.getSchema())
-					.createMappingTablesDatabase();
+      // Create new "database", i.e. new tables used for migration
+      final DatabaseMetaData mappingDatabaseMetaData = new TdmKaMappingTablesCreator(connectorRepository, SOURCE,
+          targetInfo.getSchema()).createMappingTablesDatabase();
 
-			// Add new "database"
-			connectorRepository.addConnectionInfo(TARGET_NEW_TABLES, new NewTablesConnectionInfo(mappingDatabaseMetaData));
+      // Add new "database"
+      connectorRepository.addConnectionInfo(TARGET_NEW_TABLES, new NewTablesConnectionInfo(mappingDatabaseMetaData));
 
-			// Create new tables (for performance reasons indexes will be created later)
-			createNewTables(connectorRepository, mappingDatabaseMetaData);
+      // Create new tables (for performance reasons indexes will be created later)
+      createNewTables(connectorRepository, mappingDatabaseMetaData);
 
-			setupTargetConnector(connectorRepository);
+      setupTargetConnector(connectorRepository);
 
-			setupSourceConnector(connectorRepository, mappingDatabaseMetaData);
+      setupSourceConnector(connectorRepository, mappingDatabaseMetaData);
 
-			new DefaultTableCopyTool(connectorRepository).copyTables(SOURCE, TARGET_NEW_TABLES);
+      new DefaultTableCopyTool(connectorRepository).copyTables(SOURCE, TARGET_NEW_TABLES);
 
-			removeDuplicates(connectorRepository, targetInfo);
+      removeDuplicates(connectorRepository, targetInfo);
 
-			createNewTablesIndexes(connectorRepository, mappingDatabaseMetaData);
+      createNewTablesIndexes(connectorRepository, mappingDatabaseMetaData);
 
-			new ScriptExecutorTool(connectorRepository).executeScript(TARGET, insertStatements);
+      new ScriptExecutorTool(connectorRepository).executeScript(TARGET, insertStatements);
 
-		} catch (final Exception e) {
-			LOG.error("main", e);
-		}
-	}
+    }
+    catch (final Exception e)
+    {
+      LOG.error("main", e);
+    }
+  }
 
-	private static void removeDuplicates(final ConnectorRepository connectorRepository, final TdmKaPostgresqlConnectionInfo2 targetInfo)
-			throws SQLException {
-		final List<String> removeDuplicates = new TdmKaMappingTablesDuplicateRemoverTool(targetInfo.getSchema())
-				.removeDuplicates("uuid_tdm_crash_brakeactuation", "uuid_tdm_crash_drivingspeed", "uuid_tdm_crash_features",
-						"uuid_tdm_crash_steeringangle");
-		new ScriptExecutorTool(connectorRepository).executeScript(TARGET, false, true, removeDuplicates);
-	}
+  private static void removeDuplicates(final ConnectorRepository connectorRepository,
+      final TdmKaPostgresqlConnectionInfo2 targetInfo) throws SQLException
+  {
+    final List<String> removeDuplicates = new TdmKaMappingTablesDuplicateRemoverTool(targetInfo.getSchema())
+        .removeDuplicates("uuid_tdm_crash_brakeactuation", "uuid_tdm_crash_drivingspeed", "uuid_tdm_crash_features",
+            "uuid_tdm_crash_steeringangle");
+    new ScriptExecutorTool(connectorRepository).executeScript(TARGET, false, true, removeDuplicates);
+  }
 
-	private static void createNewTablesIndexes(final ConnectorRepository connectorRepository, final DatabaseMetaData mappingDatabaseMetaData)
-			throws SQLException {
-		final DatabaseSchemaScriptCreator creator = new DatabaseSchemaScriptCreator();
-		new ScriptExecutorTool(connectorRepository).executeScript(TARGET_NEW_TABLES, creator.createIndexStatements(mappingDatabaseMetaData));
-	}
+  private static void createNewTablesIndexes(final ConnectorRepository connectorRepository,
+      final DatabaseMetaData mappingDatabaseMetaData) throws SQLException
+  {
+    final DatabaseSchemaScriptCreator creator = new DatabaseSchemaScriptCreator(mappingDatabaseMetaData);
+    new ScriptExecutorTool(connectorRepository).executeScript(TARGET_NEW_TABLES, creator.createIndexStatements());
+  }
 
-	private static void createNewTables(final ConnectorRepository connectorRepository, final DatabaseMetaData mappingDatabaseMetaData)
-			throws SQLException {
-		final DatabaseSchemaScriptCreator creator = new DatabaseSchemaScriptCreator();
-		new ScriptExecutorTool(connectorRepository).executeScript(TARGET_NEW_TABLES, creator.createTableStatements(mappingDatabaseMetaData));
-	}
+  private static void createNewTables(final ConnectorRepository connectorRepository,
+      final DatabaseMetaData mappingDatabaseMetaData) throws SQLException
+  {
+    final DatabaseSchemaScriptCreator creator = new DatabaseSchemaScriptCreator(mappingDatabaseMetaData);
+    new ScriptExecutorTool(connectorRepository).executeScript(TARGET_NEW_TABLES, creator.createTableStatements());
+  }
 
-	private static void setupSourceConnector(final ConnectorRepository connectorRepository, final DatabaseMetaData mappingDatabaseMetaData) {
-		connectorRepository.addConnectorHint(SOURCE, new OnlyTablesWithMappingsTableFilter(mappingDatabaseMetaData));
+  private static void setupSourceConnector(final ConnectorRepository connectorRepository,
+      final DatabaseMetaData mappingDatabaseMetaData)
+  {
+    connectorRepository.addConnectorHint(SOURCE, new OnlyTablesWithMappingsTableFilter(mappingDatabaseMetaData));
 
-		connectorRepository.addConnectorHint(SOURCE, new RepositoryColumnFilterHint() {
-			@Override
-			public RepositoryColumnFilter getValue() {
-				return new IdColumnsOnlyColumnFilter();
-			}
-		});
-	}
+    connectorRepository.addConnectorHint(SOURCE, new RepositoryColumnFilterHint()
+    {
+      @Override
+      public RepositoryColumnFilter getValue()
+      {
+        return new IdColumnsOnlyColumnFilter();
+      }
+    });
+  }
 
-	private static void setupTargetConnector(final ConnectorRepository connectorRepository) {
-		final IdColumnDataMapper columnDataMapper = new IdColumnDataMapper();
+  private static void setupTargetConnector(final ConnectorRepository connectorRepository)
+  {
+    final IdColumnDataMapper columnDataMapper = new IdColumnDataMapper();
 
-		connectorRepository.addConnectorHint(TARGET_NEW_TABLES, new DefaultColumnDataMapperProviderHint() {
-			@Override
-			protected void addMappings(final DefaultColumnDataMapperProvider columnDataMapperFactory) {
-				super.addMappings(columnDataMapperFactory);
-				columnDataMapperFactory.addMapping(ColumnType.CLASS_LONG, ColumnType.CLASS_STRING, columnDataMapper);
-				columnDataMapperFactory.addMapping(ColumnType.CLASS_BIGDECIMAL, ColumnType.CLASS_STRING, columnDataMapper);
-			}
-		});
+    connectorRepository.addConnectorHint(TARGET_NEW_TABLES, new DefaultColumnDataMapperProviderHint()
+    {
+      @Override
+      protected void addMappings(final DefaultColumnDataMapperProvider columnDataMapperFactory)
+      {
+        super.addMappings(columnDataMapperFactory);
+        columnDataMapperFactory.addMapping(ColumnType.CLASS_LONG, ColumnType.CLASS_STRING, columnDataMapper);
+        columnDataMapperFactory.addMapping(ColumnType.CLASS_BIGDECIMAL, ColumnType.CLASS_STRING, columnDataMapper);
+      }
+    });
 
-		connectorRepository.addConnectorHint(TARGET_NEW_TABLES, new DefaultTableMapperHint() {
-			@Override
-			public TableMapper getValue() {
-				return new TdmKaTableMapper();
-			}
-		});
+    connectorRepository.addConnectorHint(TARGET_NEW_TABLES, new DefaultTableMapperHint()
+    {
+      @Override
+      public TableMapper getValue()
+      {
+        return new TdmKaTableMapper();
+      }
+    });
 
-		connectorRepository.addConnectorHint(TARGET_NEW_TABLES, new DefaultColumnMapperHint() {
-			@Override
-			public ColumnMapper getValue() {
-				return new IdColumnsOnlyColumnFilter();
-			}
-		});
+    connectorRepository.addConnectorHint(TARGET_NEW_TABLES, new DefaultColumnMapperHint()
+    {
+      @Override
+      public ColumnMapper getValue()
+      {
+        return new IdColumnsOnlyColumnFilter();
+      }
+    });
 
-		connectorRepository.addConnectorHint(TARGET_NEW_TABLES, new RepositoryColumnFilterHint() {
-			@Override
-			public RepositoryColumnFilter getValue() {
-				return new IdColumnsOnlyColumnFilter();
-			}
-		});
-	}
+    connectorRepository.addConnectorHint(TARGET_NEW_TABLES, new RepositoryColumnFilterHint()
+    {
+      @Override
+      public RepositoryColumnFilter getValue()
+      {
+        return new IdColumnsOnlyColumnFilter();
+      }
+    });
+  }
 }
