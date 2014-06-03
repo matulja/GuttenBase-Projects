51640bbb22c054afacde073c2e8c689607c85bdb
BWXX470 <BWXX470@bwb.de>
2014-06-03 15:30:14 +0200

    new url

--- src/main/java/de/akquinet/jbosscc/guttenbase/projects/deva/MeyleMsSqlConnectionInfo.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/projects/deva/MeyleMsSqlConnectionInfo.java
@@ -3,11 +3,13 @@ package de.akquinet.jbosscc.guttenbase.projects.deva;
 import de.akquinet.jbosscc.guttenbase.connector.DatabaseType;
 import de.akquinet.jbosscc.guttenbase.connector.impl.URLConnectorInfoImpl;
 
-public class MeyleMsSqlConnectionInfo extends URLConnectorInfoImpl {
-	private static final long serialVersionUID = 1L;
+public class MeyleMsSqlConnectionInfo extends URLConnectorInfoImpl
+{
+  private static final long serialVersionUID = 1L;
 
-	public MeyleMsSqlConnectionInfo() {
-		super("jdbc:jtds:sqlserver://sap01.gaertner.com/PDP_TEST;instance=ittools", "PDP-DB-User", "pdp4meyle",
-				"net.sourceforge.jtds.jdbc.Driver", "dbo", DatabaseType.MSSQL);
-	}
+  public MeyleMsSqlConnectionInfo()
+  {
+    super("jdbc:jtds:sqlserver://SQL01-01.gaertner.com/PDP_TEST;instance=INST01", "PDP-DB-User", "pdp4meyle",
+            "net.sourceforge.jtds.jdbc.Driver", "dbo", DatabaseType.MSSQL);
+  }
 }
